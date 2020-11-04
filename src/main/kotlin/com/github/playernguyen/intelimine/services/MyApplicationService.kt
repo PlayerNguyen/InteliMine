82d2b048a7675f8731514b94da6bd1b2fec1772d
GitHub Action <action@github.com>
2020-11-04 06:53:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/playernguyen/intelimine/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/playernguyen/intelimine/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.playernguyen.intelimine.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.playernguyen.intelimine.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/playernguyen/intelimine/services/MyProjectService.kt
+++ src/main/kotlin/com/github/playernguyen/intelimine/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.playernguyen.intelimine.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.playernguyen.intelimine.MyBundle
 
 class MyProjectService(project: Project) {
 
